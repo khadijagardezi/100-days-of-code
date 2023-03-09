492ff104e59646ed71001c9e7c00daec4cb090e0
Khadija-Batool <khadijagardezi10@gmail.com>
2023-03-09 11:58:57 +0500

    Day - 001 mini project

---
+++ 001-Day/main.py
@@ -0,0 +1,9 @@
+# Get input
+city = input("where did you grow up?\n")
+pet = input("Name of your favorite pet?\n")
+
+# Generate band name
+band = city.capitalize() + " " + pet.capitalize()
+
+# Display band name
+print("Your band name could be: " + band)
